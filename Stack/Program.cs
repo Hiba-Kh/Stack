9910dbe6a4ac266c8884882e4368dcde137728df
HibaAmeen <hkhalifah95@gmail.com>
2022-01-10 14:24:03 +0200

    Implementing Stack Logic

---
+++ Stack/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Stack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var intStack = new MyStack<int>(100);
+            //intStack.Push(1);
+            //intStack.Push(2);
+            //intStack.Push(3);
+            //intStack.Push(4);
+            //intStack.Print();
+            //intStack.Pop();
+            //intStack.Peak();
+
+            for (int i = 0; i < 100; i++)
+                intStack.Push(i);
+            intStack.Print();
+            intStack.Push(1111);
+            intStack.Pop();
+            var strStack = new MyStack<string>(100);
+            strStack.Push("hi");
+            strStack.Push("a");
+            strStack.Push("b");
+            strStack.Push("c");
+            strStack.Push("d");
+            strStack.Push("Bye");
+            strStack.Print();
+            strStack.Peak();
+        }
+    }
+}
---
+++ Stack/StackBL.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class MyStack<T>
+    {
+        
+        private int maxSize;
+        public T[] objects;
+        int top;
+        public MyStack(int size)
+        {
+            top = -1;
+            maxSize = size;
+            objects = new T[maxSize];
+        }
+       
+        public bool isEmpty()
+        {
+            return top == -1;
+        }
+        public bool isFull()
+        {
+            return top == maxSize - 1;
+
+        }
+        public void Push(T data)
+        {
+            if (isFull())
+                Console.WriteLine("No Enough Space!");
+            else
+                objects[++top] = data;
+        }
+        public T Pop()
+        {
+            if (isEmpty())
+                return default(T);
+            else
+                return (T)objects[top--];
+        }
+        public void Print()
+        {
+            for (int i = 0; i <= top; i++)
+                Console.WriteLine(objects[i]);  
+        }
+        public void Peak()
+        {
+            Console.WriteLine(objects[top]);
+        }
+        public void Clear()
+        {
+            Array.Clear(objects, 0, maxSize);
+            top = -1;
+        }
+
+    }
+}
